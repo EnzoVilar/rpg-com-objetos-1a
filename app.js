e50ffa8c6a8b090aa265bbb79ffd7df1b294cabb
arthur96gg <arthur.9166@colegioprogresso.g12.br>
2026-08-31 08:37:51 -0300

    Essas são as novas regras

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 class Personagem {
-    constructor(nome, vida, ataque) {
+    constructor(nome, vida, ataque, defesa) {
         this.nome = nome,
             this.vida = vida,
-            this.ataque = ataque
+            this.ataque = ataque,
+            this.defesa = defesa
 
+    }
+
+    curar(quantidade) {
+        this.vida = this.vida + quantidade
     }
 
     causaDano(personagem) {
@@ -11,7 +16,13 @@ class Personagem {
     }
 
     recebeDano(quantidade) {
-        this.vida = this.vida - quantidade
+        let dano = quantidade - this.defesa
+
+        if (quantidade < this.defesa) {
+            dano = 0
+        }
+
+        this.vida = this.vida - dano
 
         if (this.vida <= 0) {
             this.vida = "eliminado"
@@ -29,21 +40,23 @@ class Personagem {
 
 }
 
-const guerreiro = new Personagem("Guerreiro", 80, 25)
+const guerreiro = new Personagem("Guerreiro", 80, 25, 15)
 
-const druida = new Personagem("Mago", 60, 35)
+const druida = new Personagem("Mago", 60, 35, 8)
 
-const mago = new Personagem("Gandalfe", 60, 35)
+const mago = new Personagem("Gandalfe", 60, 35, 5)
 
-const arqueiro = new Personagem("Legolas", 80, 25)
+const arqueiro = new Personagem("Legolas", 80, 25, 12)
 
-const raven = new Personagem("Raven", 40, 20)
+const raven = new Personagem("Raven", 40, 20, 5)
 
-const elara = new Personagem("Elara", 80, 40)
+const elara = new Personagem("Elara", 80, 40, 14)
 
-const dante = new Personagem("Dante", 21, 17)
+const dante = new Personagem("Dante", 21, 17, 25)
 
 druida.causaDano(mago)
 druida.causaDano(mago)
+
+mago.mostrarStatus()
 
 console.log(mago.estaVivo())
